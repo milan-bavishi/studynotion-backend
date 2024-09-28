18a00d8ab2eb2b3bff1f3d126cd321fdacee1205
milan-bavishi <milanbavishi99@gmail.com>
2024-09-28 16:07:31 +0530

    Update

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use("/api/profile",profileRoutes);
   });
   app.use("/api/products", (req, res) => {
     return res.status(200).json({
-      message: 'This is new feature change, a new route for Het'
+      message: 'This is new feature change, a new route for Milan'
     })
   });
   
